71ab62b3642c557f747c4d50f01580b02cc4c8ef
eusebioleite <eusebio.leite16@gmail.com>
2024-02-29 18:01:37 -0300

    Fixed the window title

--- app.c
+++ app.c
@@ -437,7 +437,7 @@ int main(int argc, char *argv[]) {
 
     // Create the main window
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Hello World");
+    gtk_window_set_title(GTK_WINDOW(window), "ccalc");
     gtk_window_set_default_size(GTK_WINDOW(window), 10, 10);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
